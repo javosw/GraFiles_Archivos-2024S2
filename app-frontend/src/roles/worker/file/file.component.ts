2b74ee3dbb5af70c50f6c0ce2c616a8676fa9d03
Javier Oswaldo <javoswsq@gmail.com>
2024-10-20 02:50:31 -0600

    abstraccion de la directriz mejorada

---
+++ app-frontend/src/roles/worker/file/file.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gf-file',
+  standalone: true,
+  imports: [],
+  templateUrl: './file.component.html',
+})
+export class FileComponent {
+
+}
---
+++ app-frontend/src/roles/worker/folder/folder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gf-folder',
+  standalone: true,
+  imports: [],
+  templateUrl: './folder.component.html',
+})
+export class FolderComponent {
+
+}
